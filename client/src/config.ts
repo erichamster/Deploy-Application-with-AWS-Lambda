05155fce1cab6f56a725f373294544bc5ac14980
Tumeric <datps1234@gmail.com>
2023-10-23 16:55:51 +0700

    push code

---
+++ client/src/config.ts
@@ -0,0 +1,8 @@
+const apiId = 'dazyvw1tn7'
+export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/develop`
+
+export const authConfig = {
+  domain: 'dev-pxk1s5f1ucpiuyp1.us.auth0.com',           // Auth0 domain
+  clientId: 'rYBlOsmarVmBj9wBKCpUHF86Rw4n1SFD',  // Auth0 client id
+  callbackUrl: 'http://localhost:3000/callback'
+}
